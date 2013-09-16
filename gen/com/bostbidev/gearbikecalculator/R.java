69de2315af1c2651e7393733c523e1842e8e40cb
RMH <moraleshuerva.ruben@gmail.com>
2013-09-16 18:06:10 +0200

    Better.

--- gen/com/bostbidev/gearbikecalculator/R.java
+++ gen/com/bostbidev/gearbikecalculator/R.java
@@ -9,38 +9,42 @@ package com.bostbidev.gearbikecalculator;
 
 public final class R {
     public static final class array {
-        public static final int cassete_values=0x7f050001;
-        public static final int crankset_values=0x7f050000;
+        public static final int cassete_values=0x7f060001;
+        public static final int crankset_values=0x7f060000;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int blue=0x7f040002;
+        public static final int white=0x7f040000;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btnCalculate=0x7f070006;
-        public static final int etWheel=0x7f070005;
-        public static final int spinCassette=0x7f070003;
-        public static final int spinCrankset=0x7f070001;
-        public static final int tvCassete=0x7f070002;
-        public static final int tvCrankset=0x7f070000;
-        public static final int tvResult=0x7f070007;
-        public static final int tvWheel=0x7f070004;
+        public static final int btnCalculate=0x7f080006;
+        public static final int etWheel=0x7f080005;
+        public static final int spinCassette=0x7f080003;
+        public static final int spinCrankset=0x7f080001;
+        public static final int tvCassete=0x7f080002;
+        public static final int tvCrankset=0x7f080000;
+        public static final int tvResult=0x7f080007;
+        public static final int tvWheel=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int calculate=0x7f040005;
-        public static final int cassette=0x7f040003;
-        public static final int crankset=0x7f040002;
-        public static final int error_dividir_0=0x7f040006;
-        public static final int title_activity_main=0x7f040001;
-        public static final int wheel=0x7f040004;
+        public static final int app_name=0x7f050000;
+        public static final int calculate=0x7f050004;
+        public static final int cassette=0x7f050002;
+        public static final int crankset=0x7f050001;
+        public static final int error_dividir_0=0x7f050005;
+        public static final int wheel=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }
